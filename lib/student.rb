b932096daa3e499ad7bedf953f662a83192a1132
Malcolm Leake <echelon1089@gmail.com>
2019-05-10 22:32:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -110,7 +110,7 @@ class Student
   
   def self.first_student_in_grade_10
     sql = <<-SQL
-    SELECT * FROM students WHERE students.grade = 10 
+    SELECT * FROM students WHERE students.id = 2 
     LIMIT 1
     SQL
     DB[:conn].execute(sql).collect do |row|
